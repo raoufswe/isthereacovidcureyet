9ef54d186a82a6810c5560342f2dee6f76c8f1bf
raoufswe <abdulraoufswehli58@gmail.com>
2020-05-17 07:59:57 +0800

    chore(site-config): update site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `isthereacovidcureyet`,
+    description: `get latest updates on the cure of COVID-19`,
+    author: `@raoufswe`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `isthereacovidcureyet`,
+        short_name: `isthereacovidcureyet`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
