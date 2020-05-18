75103d386c0ec3957d99c9f81670063cbf735878
Maadh <maadh@protonmail.ch>
2020-05-19 01:20:31 +0500

    changed SEO

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,54 +1,64 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
+
 const SEO = ({ post }) => {
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          title
-          description
-          url
-        }
-      }
-    }
-  `);
-
-  const defaults = data.site.siteMetadata;
-
-  const title =  defaults.title;
-  const description =  defaults.description;
-  const url = defaults.url
-  const image = defaults.image
-
+  const { site } = useStaticQuery(query)
+  const {
+    title,
+    description,
+    url,
+    image
+  } = site.siteMetadata
+  const seo = {
+    title: title,
+    description: description,
+    image: `${url}${image}`,
+    url: url,
+  }
   return (
-    <Helmet>
-      <title>{title}</title>
-      <link rel="canonical" href={url} />
-      <meta name="description" content={description} />
-      {image && <meta name="image" content={image} />}
-
-      <meta property="og:url" content={url} />
-      <meta property="og:type" content="article" />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      {image && <meta property="og:image" content={image} />}
-
+    <Helmet title={seo.title}>
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
+      {seo.url && <meta property="og:url" content={seo.url} />}
+      {seo.title && <meta property="og:title" content={seo.title} />}
+      {seo.description && (
+        <meta property="og:description" content={seo.description} />
+      )}
+      {seo.image && <meta property="og:image" content={seo.image} />}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      {image && <meta name="twitter:image" content={image} />}
+      {seo.title && <meta name="twitter:title" content={seo.title} />}
+      {seo.description && (
+        <meta name="twitter:description" content={seo.description} />
+      )}
+      {seo.image && <meta name="twitter:image" content={seo.image} />}
     </Helmet>
   )
 }
-
 export default SEO
+SEO.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  article: PropTypes.bool,
+}
+SEO.defaultProps = {
+  title: null,
+  description: null,
+  image: null,
+  article: false,
+}
+const query = graphql`
+  query SEO {
+    site {
+      siteMetadata {
+        title: title
+        description: description
+        url: url
+        image: image
+      }
+    }
+  }
+`
