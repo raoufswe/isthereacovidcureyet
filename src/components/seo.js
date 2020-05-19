4e5c17993b146c2671c62efcda0a991c1022d141
Maadh <maadh@protonmail.ch>
2020-05-19 16:40:09 +0500

    fixed images

--- src/components/seo.js
+++ src/components/seo.js
@@ -4,40 +4,56 @@ import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
 const SEO = ({ post }) => {
-  SEO.propTypes = {
-    title: PropTypes.string,
-    description: PropTypes.string,
-    image: PropTypes.string,
-    article: PropTypes.bool
+  const { site } = useStaticQuery(query)
+  const { title, description, url, image } = site.siteMetadata
+
+  const seo = {
+    title: title,
+    description: description,
+    image: `${url}${image}`,
+    url: url,
   }
-  SEO.defaultProps = {
-    title: null,
-    description: null,
-    image: null,
-    article: false
-  }
-  const query = graphql`
+  return (
+    <Helmet title={seo.title}>
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
+      {seo.url && <meta property="og:url" content={seo.url} />}
+      {seo.title && <meta property="og:title" content={seo.title} />}
+      {seo.description && (
+        <meta property="og:description" content={seo.description} />
+      )}
+      {seo.image && <meta property="og:image" content={seo.image} />}
+      <meta name="twitter:card" content="summary_large_image" />
+      {seo.title && <meta name="twitter:title" content={seo.title} />}
+      {seo.description && (
+        <meta name="twitter:description" content={seo.description} />
+      )}
+      {seo.image && <meta name="twitter:image" content={seo.image} />}
+    </Helmet>
+  )
+}
+export default SEO
+SEO.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  article: PropTypes.bool,
+}
+SEO.defaultProps = {
+  title: null,
+  description: null,
+  image: null,
+  article: false,
+}
+const query = graphql`
   query SEO {
     site {
       siteMetadata {
+        title: title
+        description: description
         url: url
         image: image
       }
     }
   }
 `
-
-  const { site } = useStaticQuery(query)
-  const { title, description, url, image } = site.siteMetadata
-
-  const seo = {
-    image: `${url}${image}`,
-    url: url
-  }
-  return (
-    <Helmet title={seo.title}>
-      {seo.image && <meta name="twitter:image" content={seo.image}/>}
-    </Helmet>
-  )
-}
-export default SEO
